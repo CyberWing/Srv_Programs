ba0c9ac53f265e681ecebb60062ca29598d3a8d2
phyominhtun1990 <phyominhtun1990@gmail.com mailto:phyominhtun1990@gmail.com>
2015-08-25 15:56:34 +0630

    curl_add

--- Shell_Scripts/chk_http_code.sh
+++ Shell_Scripts/chk_http_code.sh
@@ -2,7 +2,7 @@
 
 echo "This script can check HTTP Header for website"
 echo "#############################################"
-read -p "Type URL for check HTTP Header: " URL
-#echo -n "Type URL for check HTTP Header" && read URL
+#read -p "Type URL for check HTTP Header: " URL
+echo -n "Type URL for check HTTP Header" && read URL
 echo "Output HTTP Header Result Is = " `curl -o /dev/null --silent --head --write-out '%{http_code}\n' $URL`
 
